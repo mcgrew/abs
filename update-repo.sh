1afe8763f316f5436a56143dd7859b9b61941296
Thomas McGrew <tjmcgrew@gmail.com>
2012-05-31 15:14:42 -0400

    Added the following packages: 	android-sdk-platform-tools 	condor 	fastboot 	mrxvt 	revolutionary 	terminus-font-ttf 	yuicompressor Updated the following packages: 	chromium (backporting pulseaudio glitch fix) 	linux-ralus Removed the following packages: 	vim (not the culprit in the performance issues) Added a script for updating the arch repo db

---
+++ update-repo.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+REPO_NAME="mcgrew"
+REPO_DIR="/var/www/html/archlinux/$REPO_NAME/os/x86_64"
+
+# nicenesses range from -20 (most favorable scheduling) to 19 (least favorable)
+NICE=19
+
+# 0 for none, 1 for real time, 2 for best-effort, 3 for idle
+IONICE_CLASS=2
+
+# 0-7 (for IONICE_CLASS 1 and 2 only), 0=highest, 7=lowest
+IONICE_PRIORITY=7
+
+
+DB_FILE="${REPO_NAME}.db"
+UPDATE_REPO="/usr/bin/repo-add ${REPO_DIR}/${DB_FILE}.tar.gz ${REPO_DIR}/*.pkg.tar.*"
+
+if [ -x /usr/bin/nice ]; then
+  UPDATE_REPO="/usr/bin/nice -n ${NICE:-19} ${UPDATE_REPO}"
+fi
+
+if [ -x /usr/bin/ionice ]; then
+  UPDATE_REPO="/usr/bin/ionice -c ${IONICE_CLASS:-2} -n ${IONICE_PRIORITY:-7} ${UPDATE_REPO}"
+fi
+
+if [ ! -z "$REPO_DIR" ]; then
+	cd "$REPO_DIR"
+	newest="$(ls -1c | head -1)"
+	# Update the repo database if it is not the newest file
+	if [ ! -z "${DB_FILE}" ]; then
+		if [ "${newest:0:${#DB_FILE}}" != "${DB_FILE}" ]; then
+				rm ${DB_FILE}*
+				${UPDATE_REPO}
+		else
+			echo "Repo database already up to date."
+		fi;
+	else
+		echo "Configuration error dectected in $0";
+	fi;
+fi;
+
